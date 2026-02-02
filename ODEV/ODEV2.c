48c19038d802b05e3fe14eb790eb89aa033c5888
mberkebal <mbb.berkebal@gmail.com>
2026-02-02 20:17:05 +0300

    Update ODEV2.c

--- ODEV/ODEV2.c
+++ ODEV/ODEV2.c
@@ -3,22 +3,22 @@
 
 Aşağıdaki cümlelerin doğru ya da yanlış olduğunu yazınız:
 
-Otomatik ömürlü nesnelerin çöp değerleri (garbage value) ile kullanılması tanımsız davranıştır.
+Otomatik ömürlü nesnelerin çöp değerleri (garbage value) ile kullanılması tanımsız davranıştır. doğru
 
-Tam sayı ve gerçek sayı türlerinden statik ömürlü değişkenler ilk değer verilmeden tanımlansalar dahi hayata 0 değeri ile başlarlar.
+Tam sayı ve gerçek sayı türlerinden statik ömürlü değişkenler ilk değer verilmeden tanımlansalar dahi hayata 0 değeri ile başlarlar. doğru
 
-long long türü en az 4 byte olmak zorundadır.
+long long türü en az 4 byte olmak zorundadır. yanlış(8 bayt)
 
-char türünün işaretli ya da işaretsiz olması derleyiciye bağlıdır (implementation defined)
+char türünün işaretli ya da işaretsiz olması derleyiciye bağlıdır (implementation defined) doğru
 
-işaretli tamsayı türlerinde taşma tanımsız davranıştır (undefined behavior).
+işaretli tamsayı türlerinde taşma tanımsız davranıştır (undefined behavior). 
 
 işaretsiz tamsayı türlerinde taşma tanımsız davranıştır (undefined behavior).
 
-int x; biçiminde bildirilen x değişkeni işaretli int türündendir.
+int x; biçiminde bildirilen x değişkeni işaretli int türündendir. doğru 
 
-long türünden bir değişken int türünden bir değişkene göre daha büyük tam sayıları tutabilir.
+long türünden bir değişken int türünden bir değişkene göre daha büyük tam sayıları tutabilir. doğru
 
-signed x; biçiminde bildirilen x değişkeni işaretli char türündendir.
+signed x; biçiminde bildirilen x değişkeni işaretli char türündendir. yanlış(int)
 
 */
