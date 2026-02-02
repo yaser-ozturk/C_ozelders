cbb0396deb03305606a9cb9676abed36e899ac64
mberkebal <mbb.berkebal@gmail.com>
2026-02-02 20:04:32 +0300

    Update ODEV1.c

--- ODEV/ODEV1.c
+++ ODEV/ODEV1.c
@@ -1,31 +1,31 @@
 /*
 // geçerli bildirimler nelerdir neden  ?
-int main(void)
+int main(void) 
 {
-signed x1;
-unsigned double x2;
-long int long x3;
-bool x4;
-char int x5;
-x6 float;
-long double x7;
-unsigned short int x8;
-long unsigned x9;
-long float x10;
+signed x1; 
+unsigned double x2; geçersiz böyle bir tür yok
+long int long x3; 
+bool x4; bool true ve false için kullanılır geçersiz
+char int x5; char ve int 2 farklı tür geçersiz
+x6 float; float x6 olması gerek geçersiz
+long double x7; long double bir tür geçerli
+unsigned short int x8; işaretsiz oldugu belirtilen bir tür yazılmış geçerli
+long unsigned x9; geçerli tür ve işareti belirtilmiş
+long float x10; geçersiz böyle bir tür yok
 }
 
 
 int main(void)
 {
-    int _6542;
-    unsigned check;
-    long long_distance;
-    int 4thvalue;
-    short too_short;
-    signed binary;
-    int register;
-    double BURGER;
-    int int_x;
+    int _6542; geçerli int türü ile değişken tanımlanmış
+    unsigned check; 
+    long long_distance; geçerli çünkü long türü ile bir değişken tanımlaması yapılmış
+    int 4thvalue; geçersiz 4 ile başladıgı için tanımsız oluyor
+    short too_short; geçerli çünkü short türü ile bir değişken tanımlaması yapılmış
+    signed binary; işaretli oldugu belirtilen bir değişken tanımlanmış yani geçerli 
+    int register; geçerli int türü ile değişken tanımlanmış
+    double BURGER;  BURGER adlı değişken double türü ile tanımlanmış yani geçerli
+    int int_x; int_x değişken,int de tür yani geçerli
 }
 
 
