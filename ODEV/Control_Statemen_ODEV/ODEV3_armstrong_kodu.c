3ebef3ec8001ced49c2b5592a1be08d6580b71b9
mberkebal <mbb.berkebal@gmail.com>
2026-02-08 14:32:45 +0300

    Update ODEV3_armstrong_kodu.c

--- ODEV/Control_Statemen_ODEV/ODEV3_armstrong_kodu.c
+++ ODEV/Control_Statemen_ODEV/ODEV3_armstrong_kodu.c
@@ -1,5 +1,23 @@
-// n basamaklı bir sayının,
-// basamaklarının n. kuvvetlerinin toplamı kendisine
-// eşitse bu sayıya Armstrong sayısı denir.
+for (int i = 999999999 ; i >=100000000 ; i--) {
 
-// 9 basamaklı en büyük Armstrong sayısını hesaplayan bir C programı yazınız.
+        int gecicisayi = i;
+        long long toplam = 0;
+
+        while (gecicisayi>0) {
+
+            int basamak = gecicisayi % 10;
+            long long çarpım =1;
+
+            for (int k=0; k<9 ; k++) {
+                çarpım = çarpım * basamak;
+            }
+
+            toplam = toplam + çarpım;
+            gecicisayi = gecicisayi / 10;
+
+        }
+        if (toplam == i) {
+            printf("En buyuk 9 basamaklı Armstrong sayısı: %d",i);
+            break;
+        }
+    }
