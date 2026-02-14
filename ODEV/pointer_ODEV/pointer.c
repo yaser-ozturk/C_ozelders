fc9e17c6ed2d0caef5ff81bd0d5bcd752d27167a
mberkebal <mbb.berkebal@gmail.com>
2026-02-14 12:26:39 +0300

    Update pointer.c

--- ODEV/pointer_ODEV/pointer.c
+++ ODEV/pointer_ODEV/pointer.c
@@ -5,38 +5,38 @@
 Aşağıdaki deyimlerin C dilinde geçerli olup olmadığını yazınız. Her bir sentaks hatasının nedenini açıklayınız:
 int main(void)
 {
-    int a[] = { 1, 3, 5 };
-    int b[] = { 2, 4, 6 };
-    int* p = a;
+    int a[] = { 1, 3, 5 };  geçerli
+    int b[] = { 2, 4, 6 };  geçerli
+    int* p = a; geçerli
 
-    a = b;
-    ++a;
-    p = b;
-    *p = *a;
-    *a = *b;
-    ++* a;
-    ++* p;
-    (*p)++;
-    a = p;
+    a = b; geçersiz (diziler bu şekilde birbirine eşitlenemez)
+    ++a; geçersiz(dizi bu şekilde arttırılamaz)
+    p = b; geçerli
+    *p = *a; geçerli
+    *a = *b; geçerli
+    ++* a; geçerli
+    ++* p; geçerli
+    (*p)++; geçerli
+    a = p; geçersiz
 }
 
 Aşağıdaki deyimlerin C dilinde geçerli olup olmadığını yazınız. Her bir sentaks hatasının nedenini açıklayınız:
 int main(void)
 {
-    int x = 10;
-    int y = 10;
-    int* p = &x;
-    int* q = &y;
+    int x = 10; geçerli
+    int y = 10; geçerli
+    int* p = &x; geçerli
+    int* q = &y; geçerli
 
-    &x = &y;
-    x = *p;
-    *p = *q;
-    p = &y;
-    p = q;
-    &x++;
-    ++* p;
-    &++x;
-    &y = p;
+    &x = &y; geçersiz( 2 tane rvalue eşitlenemez)
+    x = *p; geçerli
+    *p = *q; geçerli
+    p = &y; geçerli
+    p = q; geçerli
+    &x++; geçersiz 
+    ++* p; geçerli
+    &++x; geçersiz 
+    &y = p; geçersiz(r value)
 }
 
 
